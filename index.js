4b32de399293c9d7e8f30021cca6c6e3702b5bff
Joel Lim <103879u@gmail.com>
2017-10-16 22:27:54 +0800

    refactor index.js

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-"use strict"
+"use strict";
 
-var fs = require('fs')
-var commander = require('commander')
+var commander = require("commander");
+var Parser = require("./Parser.js");
 
-commander.version('0.1.0')
-.option('-d, --directory <directory>', 'Directory where json file(s) are located')
-.parse(process.argv)
+commander
+  .version("0.1.0")
+  .option(
+    "-d, --directory <directory>",
+    "Directory where json file(s) are located"
+  )
+  .parse(process.argv);
 
-var directory = commander.directory
-
-if(!directory){
-  console.error("INVALID_DIRECTORY")
-  process.exit()
-}
+var parser = new Parser();
+parser.parseDirectory(commander.directory).then(
+  dir => {
+    console.log(dir);
+  },
+  err => {
+    console.error(err);
+  }
+);
