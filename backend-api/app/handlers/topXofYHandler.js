e1e8e1fe55949694cefc703e60b7a02c3a7046c9
Chan Lup Seng <lupsengchan@gmail.com>
2017-10-31 04:27:52 +0800

    able to get topN Author from Y from mongoose

--- backend-api/app/handlers/topXofYHandler.js
+++ backend-api/app/handlers/topXofYHandler.js
@@ -1,67 +1,86 @@
-
 /**
  * Returns the topN authors.
  *
  * @param {integer} topN number of authors (default is 10)
  */
 function getTopAuthors(options) {
+
+  return new Promise((resolve, reject) => {
     options = options || {};
     const topN = options.topN || 10;
-  
-    const authors = papersController.getAuthorsObject();
-    let topAuthors = papersController.group({
-      groupsFromPaper: paper =>
-        paper.getAuthors().map(author => author.getId() || author.getName()),
-      paperFilter: options.paperFilter
-    });
-  
-    topAuthors = Object.keys(topAuthors)
-      .sort((author1, author2) =>
-        topAuthors[author2].length - topAuthors[author1].length)
-      .slice(0, topN)
-      .map(authorId => {
-        return {
-          author: authors[authorId],
-          count: topAuthors[authorId].length,
-          papers: topAuthors[authorId]
-        };
-      });
-  
-    return topAuthors;
-  }
-  
-module.exports = function(options){
-    logger = options.logger
-    papersController = options.papersController
-    db = options.db
 
-    return (req, res) => {
-      db.find({ 'venue': 'icse' }, 'title year',function (err, papers) {
-        if (err) return logger.info(err);
-        papers.forEach(function(element) {          
-        logger.info(element.title);
+    y = options.y;
+    value = options.value;
+    var filterY = {};
+    filterY[y] = value;
+
+    var topAuthors = {};
+    var authorsObjArr = {};
+    var paperObj = {};
+
+    db.find(filterY, function (err, papers) {
+      if (err) return logger.info(err);
+      papers.forEach(function (element) {
+        paperObj['title'] = element.title;
+        paperObj['authors'] = element.authors;
+        paperObj['venue'] = element.venue;
+        paperObj['inCitations'] = element.inCitations;
+        paperObj['outCitations'] = element.outCitations;
+        paperObj['year'] = element.year;
+        paperObj['abstract'] = element.abstract;
+        paperObj['keyPhrases'] = element.keyPhrases;
+        element.authors.forEach(authorObj => {
+          id = authorObj.ids[0];
+          topAuthors[id] = topAuthors[id] || [];
+          topAuthors[id].push(paperObj);
+          authorsObjArr[id] = authorObj;
         }, this);
-      })
-      const params = req.query;
-    
-      const options = {};
-      const paperFilters = [];
-    
-      if (params.venue) {
-        paperFilters.push(paper => paper.getVenue().toLowerCase() === params.venue.toLowerCase());
       }
-    
-      if (params.topN) {
-        options.topN = params.topN;
-      }
-    
-      if (paperFilters.length > 0) {
-        options.paperFilter = paper =>
-          paperFilters.every(paperFilter => paperFilter(paper));
-      }
-    
-      res.send(JSON.stringify(getTopAuthors(options)));
+      );
+
+      topAuthors = Object.keys(topAuthors)
+        .sort((author1, author2) =>
+          topAuthors[author2].length - topAuthors[author1].length)
+        .slice(0, topN)
+        .map(authorId => {
+          return {
+            author: authorsObjArr[authorId],
+            count: topAuthors[authorId].length,
+            papers: topAuthors[authorId]
+          };
+
+        });
+      resolve(topAuthors);
+    });
+  });
+}
+
+module.exports = function (options) {
+  logger = options.logger
+  papersController = options.papersController
+  db = options.db
+
+  return (req, res) => {
+    const params = req.query;
+
+    const options = {};
+    const paperFilters = [];
+
+    if (params.y && params.value) {
+      options.y = params.y;
+      options.value = params.value;
     }
-     
+
+    if (params.topN) {
+      options.topN = params.topN;
+    }
+
+    if (paperFilters.length > 0) {
+      options.paperFilter = paper =>
+        paperFilters.every(paperFilter => paperFilter(paper));
+    }
+
+    getTopAuthors(options).then(result => res.send(JSON.stringify(result)));
   }
-  
+
+}
