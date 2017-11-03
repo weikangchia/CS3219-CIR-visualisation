349cd27dcd95193fb256914547f14a041c097ad2
Wei Kang <weikangchia@gmail.com>
2017-11-03 22:32:27 +0800

    Fixed port

--- frontend/app.js
+++ frontend/app.js
@@ -52,5 +52,5 @@ app.use('/graphs-incitations', (req, res) => {
 });
 
 const server = app.listen(process.env.PORT, () => {
-  logger.info(`Listening on port ${server.address().process.env.PORT}`);
+  logger.info(`Listening on port ${server.address().port}`);
 });
