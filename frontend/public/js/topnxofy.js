871d38facd3e87a673d0458c22aa489505d175d6
Joel Lim <103879u@gmail.com>
2017-11-05 22:28:49 +0800

    basic UI

---
+++ frontend/public/js/topnxofy.js
@@ -0,0 +1,62 @@
+/**
+ * Angular app for top n x of y
+ */
+
+var app = angular.module("topnxofy", []);
+
+app.controller("QueryDisplayController", function($scope) {});
+
+app.controller("QueryParamsController", function($scope) {
+  $scope.xCategories = [
+    {
+      display: "Authors",
+      key: "authors"
+    },
+    {
+      display: "Conferences",
+      key: "conference"
+    },
+    {
+      display: "Citations",
+      key: "citation"
+    },
+    {
+      display: "Venues",
+      key: "venue"
+    },
+    {
+      display: "Book Titles",
+      key: "book-title"
+    },
+    {
+      display: "Base Papers",
+      key: "base-papers"
+    }
+  ];
+  $scope.yCategories = [
+    {
+      display: "Author",
+      key: "authors"
+    },
+    {
+      display: "Conference",
+      key: "conference"
+    },
+    {
+      display: "Cited Author",
+      key: "cited-author"
+    },
+    {
+      display: "Cited By Author",
+      key: "cited-by-author"
+    }
+  ];
+
+  $scope.topN = 0;
+  $scope.query = {};
+  $scope.show = true;
+
+  $scope.submitQuery = function() {
+    console.log($scope.query);
+  };
+});
