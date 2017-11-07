fc9245f5fc8515ad0128a3271c3fa478b438f125
Joel Lim <103879u@gmail.com>
2017-11-07 21:52:16 +0800

    init autocomplete

--- frontend/public/js/topnxofy.js
+++ frontend/public/js/topnxofy.js
@@ -2,61 +2,76 @@
  * Angular app for top n x of y
  */
 
-cir.controller("QueryDisplayController", function($scope) {
+cir.controller("QueryParamsController", [
+  "API_HOST", "$scope",
+  function(API_HOST, $scope) {
+    $scope.xCategories = [
+      {
+        display: "Authors",
+        key: "authors"
+      },
+      {
+        display: "Conferences",
+        key: "conference"
+      },
+      {
+        display: "Citations",
+        key: "citation"
+      },
+      {
+        display: "Venues",
+        key: "venue"
+      },
+      {
+        display: "Book Titles",
+        key: "book-title"
+      },
+      {
+        display: "Base Papers",
+        key: "base-papers"
+      }
+    ];
+    $scope.yCategories = [
+      {
+        display: "Author",
+        key: "authors"
+      },
+      {
+        display: "Venue",
+        key: "venue"
+      },
+      {
+        display: "Cited Author",
+        key: "cited-author"
+      },
+      {
+        display: "Cited By Author",
+        key: "cited-by-author"
+      }
+    ];
 
-});
+    $scope.query = {
+      topN : 3
+    };
 
-cir.controller("QueryParamsController", function($scope) {
-  $scope.xCategories = [
-    {
-      display: "Authors",
-      key: "authors"
-    },
-    {
-      display: "Conferences",
-      key: "conference"
-    },
-    {
-      display: "Citations",
-      key: "citation"
-    },
-    {
-      display: "Venues",
-      key: "venue"
-    },
-    {
-      display: "Book Titles",
-      key: "book-title"
-    },
-    {
-      display: "Base Papers",
-      key: "base-papers"
-    }
-  ];
-  $scope.yCategories = [
-    {
-      display: "Author",
-      key: "authors"
-    },
-    {
-      display: "Conference",
-      key: "conference"
-    },
-    {
-      display: "Cited Author",
-      key: "cited-author"
-    },
-    {
-      display: "Cited By Author",
-      key: "cited-by-author"
-    }
-  ];
+    $scope.submitQuery = function() {
+      console.log($scope.query);
+    };
 
-  $scope.topN = 0;
-  $scope.query = {};
-  $scope.show = true;
-
-  $scope.submitQuery = function() {
-    console.log($scope.query);
-  };
-});
+    $(".domain-selector").autocomplete({
+      source: function(req, resolve) {
+        var domain = $scope.query.yCategoryValue
+        var search = $scope.query.yValue
+        var searchParams = new URLSearchParams();
+        searchParams.set('domain', domain);
+        searchParams.set('search', search);
+        var url = API_HOST + "/autocomplete?" + searchParams.toString()
+        console.log(url)
+        $.ajax({
+          url: url,
+          success: resolve
+        });
+      }
+    });
+  }
+]);
