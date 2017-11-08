48211d75e490224e5d714b1d11b50e386c1dc5b4
Joel Lim <103879u@gmail.com>
2017-11-09 04:05:42 +0800

    add some util methods

--- frontend/public/js/utils.js
+++ frontend/public/js/utils.js
@@ -2,6 +2,26 @@ const API_HOST = "http://localhost:3000";
 const FRONTEND_HOST = "http://localhost:9000";
 
 /**
+ * See https://paulund.co.uk/capitalize-first-letter-string-javascript
+ * @param {*} string 
+ */
+function capitalize(string) 
+{
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+/**
+ * See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+ * @param {*} min 
+ * @param {*} max 
+ */
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+}
+
+/**
  * Initialize the fakeloader animation.
  */
 function initFakeLoader() {
