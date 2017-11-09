8afde1532f446a982e3b03fc51951bcc6fbd06e5
Wei Kang <weikangchia@gmail.com>
2017-11-09 15:42:08 +0800

    Refactor code

--- backend-api/app/handlers/graphIncitationHandler.js
+++ backend-api/app/handlers/graphIncitationHandler.js
@@ -54,7 +54,7 @@ async function dig(paper, currentLevel, maxLevel, basePaperId, nodeLinks) {
   }
 
   if (currentLevel >= maxLevel) {
-    logger.info(`handler="${handlerName}" currentLevel=${currentLevel} message="level reached"`);
+    logger.debug(`handler="${handlerName}" currentLevel=${currentLevel} message="level reached"`);
   } else {
     const {
       inCitations
@@ -64,8 +64,8 @@ async function dig(paper, currentLevel, maxLevel, basePaperId, nodeLinks) {
       const inCitedPaper = await getPaperById(inCitationId, paperProjOptions);
 
       if (inCitedPaper === null) {
-        /* logger.info(`handler="${handlerName}" currentLevel=${currentLevel} citationId="${inCitationId}" ` +
-          `message="null (skipping)" parentId="${basePaperId}"`); */
+        logger.debug(`handler="${handlerName}" currentLevel=${currentLevel} citationId="${inCitationId}" ` +
+          `message="null (skipping)" parentId="${basePaperId}"`);
       } else {
         nodeLinks.rawNodes[inCitedPaper.id] = parsePaper(inCitedPaper);
 
@@ -76,8 +76,8 @@ async function dig(paper, currentLevel, maxLevel, basePaperId, nodeLinks) {
             target: paper.id
           });
 
-        /* logger.info(`handler="${handlerName}" currentLevel=${currentLevel} citationId="${inCitationId}" ` +
-          `incitationLength=${inCitedPaper.inCitations.length} message="continue to dig" parentId="${basePaperId}"`); */
+        logger.debug(`handler="${handlerName}" currentLevel=${currentLevel} citationId="${inCitationId}" ` +
+          `incitationLength=${inCitedPaper.inCitations.length} message="continue to dig" parentId="${basePaperId}"`);
 
         await dig(inCitedPaper, currentLevel + 1, maxLevel, inCitationId, nodeLinks);
       }
@@ -87,40 +87,52 @@ async function dig(paper, currentLevel, maxLevel, basePaperId, nodeLinks) {
   return nodeLinks;
 }
 
-function handler(options) {
-  return (req, res) => {
-    ({
-      logger,
-      db
-    } = options);
+/**
+ * Returns an array of node and links to build incitation graph.
+ *
+ * @param {string} title
+ * @param {int} level
+ */
+async function getIncitationGraph(title, level) {
+  const nodeLinks = {
+    rawNodes: {},
+    nodes: [],
+    links: []
+  };
 
+  await getPaperByTitle(title, paperProjOptions).then(paper => {
+    if (paper === null) {
+      logger.info(`handler="${handlerName}" message="unable to find paper ${title}"`);
+    } else {
+      logger.info(`handler="${handlerName}" message="searching for graph incitation for ${title}"`);
+      dig(paper, 0, level, null, nodeLinks).then(result => {
+        Object.keys(nodeLinks.rawNodes).forEach(key => {
+          nodeLinks.nodes.push(nodeLinks.rawNodes[key]);
+        });
+
+        delete nodeLinks.rawNodes;
+      });
+    }
+  });
+
+  return nodeLinks;
+}
+
+function handler(options) {
+  ({
+    logger,
+    db
+  } = options);
+
+  return (req, res) => {
     const params = req.query;
     const title = params.title.trim() || "";
     const level = parseInt(params.level, 10) || 2;
 
     logger.info(`handler="${handlerName}" title="${title}" maxLevel=${level}`);
 
-    getPaperByTitle(params.title, paperProjOptions).then(paper => {
-      const nodeLinks = {
-        rawNodes: {},
-        nodes: [],
-        links: []
-      };
-
-      if (paper === null) {
-        logger.info(`handler="${handlerName}" message="unable to find paper ${title}"`);
-        res.send(nodeLinks);
-      } else {
-        dig(paper, 0, level, null, nodeLinks).then(result => {
-          Object.keys(nodeLinks.rawNodes).forEach(key => {
-            nodeLinks.nodes.push(nodeLinks.rawNodes[key]);
-          });
-
-          delete nodeLinks.rawNodes;
-
-          res.send(JSON.stringify(result));
-        });
-      }
+    getIncitationGraph(title, level).then(result => {
+      res.send(JSON.stringify(result));
     });
   };
 }
