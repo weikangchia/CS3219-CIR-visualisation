689a184be8713940c52ebb4b8ae35033efb40841
Joel Lim <103879u@gmail.com>
2017-11-09 17:18:06 +0800

    remove Loggly in test environment

--- backend-api/app/middleware/logger.js
+++ backend-api/app/middleware/logger.js
@@ -4,22 +4,26 @@ require("winston-loggly");
 
 dotenv.load();
 
-const logger = new winston.Logger({
-  transports: [
-    new winston.transports.Console({
-      level: process.env.LOG_LEVEL,
-      timestamp: () => {
-        return (new Date()).toISOString();
-      }
-    }),
-    new winston.transports.Loggly({
-      inputToken: process.env.LOGGLY_INPUT_TOKEN,
-      subdomain: process.env.LOGGLY_SUBDOMAIN,
-      tags: ["cs3219-backend-api"],
-      json: true,
-      level: process.env.LOG_LEVEL
-    })
-  ]
-});
+if (process.env.NODE_ENV === "test") {
+  const logger = new winston.Logger();
+} else {
+  const logger = new winston.Logger({
+    transports: [
+      new winston.transports.Console({
+        level: process.env.LOG_LEVEL,
+        timestamp: () => {
+          return new Date().toISOString();
+        }
+      }),
+      new winston.transports.Loggly({
+        inputToken: process.env.LOGGLY_INPUT_TOKEN,
+        subdomain: process.env.LOGGLY_SUBDOMAIN,
+        tags: ["cs3219-backend-api"],
+        json: true,
+        level: process.env.LOG_LEVEL
+      })
+    ]
+  });
+}
 
 module.exports = logger;
