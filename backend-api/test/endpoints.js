703e25aac698f76b05aa7ae8e770d0381d7b07f9
Joel Lim <103879u@gmail.com>
2017-11-09 15:38:41 +0800

    init first tests with autocomplete

---
+++ backend-api/test/endpoints.js
@@ -0,0 +1,26 @@
+//During the test the env variable is set to test
+process.env.NODE_ENV = "test";
+process.env.PORT = 3001;
+
+let mongoose = require("mongoose");
+
+//Require the dev-dependencies
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let server = require("../app");
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+const tests = [
+  require('./endpoints/autocomplete')
+]
+
+tests.forEach(test => {
+  test({
+    chai,
+    chaiHttp,
+    server,
+    should
+  })
+})
