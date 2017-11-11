5efdf826123905b175ff64c5d78ce1767760a1f3
Wei Kang <weikangchia@gmail.com>
2017-11-11 13:54:34 +0800

    Refactor code

--- frontend/public/js/cir.js
+++ frontend/public/js/cir.js
@@ -2,7 +2,7 @@
  * Global angular app
  */
 
-var cir = angular.module("CIR", []);
+const cir = angular.module("CIR", []);
 
 cir.constant('API_HOST', API_HOST);
 cir.constant('FRONTEND_HOST', FRONTEND_HOST);
