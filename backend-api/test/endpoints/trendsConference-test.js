27b74336ebf3f08aaec3eb134b0fe0c07642ce1e
Wei Kang <weikangchia@gmail.com>
2017-11-12 16:17:03 +0800

    Refactor endpoint test

---
+++ backend-api/test/endpoints/trendsConference-test.js
@@ -0,0 +1,77 @@
+module.exports = function (options) {
+  const {
+    sinon,
+    chai,
+    chaiHttp,
+    mongoose,
+    server
+  } = options;
+
+  const invalidFieldCode = 400;
+  const invalidFieldMessage = "Missing field/invalid field.";
+
+  let Paper;
+  beforeEach(() => {
+    Paper = mongoose.connection.model("Paper");
+    sinon.stub(Paper, "aggregate");
+  });
+
+  afterEach(() => {
+    Paper.aggregate.restore();
+  });
+
+  it("it should return 400 for empty name", done => {
+    chai
+      .request(server)
+      .get("/trends/conference")
+      .end((err, res) => {
+        res.should.have.status(400);
+        res.body.should.be.a("object");
+        res.body.error.code.should.equal(invalidFieldCode);
+        res.body.error.message.should.equal(invalidFieldMessage);
+        done();
+      });
+  });
+
+  it("it should use default minYear and maxYear", done => {
+    const expectedResult = [];
+
+    Paper.aggregate.yields(null, expectedResult);
+
+    chai
+      .request(server)
+      .get("/trends/conference?name=icse")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("array");
+        res.body.length.should.equal(1);
+        res.body[0].year.should.equal(1800);
+        res.body[0].count.should.equal(0);
+        done();
+      });
+  });
+
+  it("it should GET an array of conference trends", done => {
+    const expectedResult = [{
+        count: 1,
+        year: 2015
+      },
+      {
+        count: 1,
+        year: 2016
+      }
+    ];
+
+    Paper.aggregate.yields(null, expectedResult);
+
+    chai
+      .request(server)
+      .get("/trends/conference?name=icse&minYear=2015&maxYear=2017")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("array");
+        res.body.length.should.equal(3);
+        done();
+      });
+  });
+};
---
+++ backend-api/test/endpoints/trendsKeyPhrase-test.js
@@ -0,0 +1,112 @@
+module.exports = function (options) {
+  const {
+    sinon,
+    chai,
+    chaiHttp,
+    mongoose,
+    server
+  } = options;
+
+  const invalidFieldCode = 400;
+  const invalidFieldMessage = "Missing field/invalid field.";
+
+  let Paper;
+
+  beforeEach(() => {
+    Paper = mongoose.connection.model("Paper");
+    sinon.stub(Paper, "find");
+  });
+
+  afterEach(() => {
+    Paper.find.restore();
+  });
+
+  it("it should GET an array of keyphrase trends", done => {
+    const expectedResult = [{
+        _id: "59f32bd4cd71fe9b08c653e9",
+        year: 2015
+      },
+      {
+        _id: "59f32bd5cd71fe9b08c6691a",
+        year: 2015
+      },
+      {
+        _id: "59f32bd5cd71fe9b08c6837c",
+        year: 2016
+      }
+    ];
+
+    Paper.find.yields(null, expectedResult);
+
+    chai
+      .request(server)
+      .get("/trends/keyphrase?phrase=abc&minYear=2015&maxYear=2017")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("array");
+        res.body.length.should.equal(3);
+
+        res.body[0].year.should.equal("2015");
+        res.body[0].count.should.equal(2);
+
+        res.body[1].year.should.equal("2016");
+        res.body[1].count.should.equal(1);
+
+        res.body[2].year.should.equal("2017");
+        res.body[2].count.should.equal(0);
+        done();
+      });
+  });
+
+  it("it should return 400 for empty phrase", done => {
+    chai
+      .request(server)
+      .get("/trends/keyphrase")
+      .end((err, res) => {
+        res.should.have.status(400);
+        res.body.should.be.a("object");
+        res.body.error.code.should.equal(invalidFieldCode);
+        res.body.error.message.should.equal(invalidFieldMessage);
+        done();
+      });
+  });
+
+  it("it should return 200 for empty year", done => {
+    Paper.find.yields(null, []);
+    chai
+      .request(server)
+      .get("/trends/keyphrase?phrase=abc")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.length.should.equal(1);
+
+        res.body[0].year.should.equal("1800");
+        res.body[0].count.should.equal(0);
+        done();
+      });
+  });
+
+  it("it should return 200 for empty minYear", done => {
+    Paper.find.yields(null, []);
+    chai
+      .request(server)
+      .get("/trends/keyphrase?phrase=abc&maxYear=1803")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.length.should.equal(4);
+
+        res.body[0].year.should.equal("1800");
+        res.body[0].count.should.equal(0);
+
+        res.body[1].year.should.equal("1801");
+        res.body[1].count.should.equal(0);
+
+        res.body[2].year.should.equal("1802");
+        res.body[2].count.should.equal(0);
+
+        res.body[3].year.should.equal("1803");
+        res.body[3].count.should.equal(0);
+        done();
+      });
+  });
+};
